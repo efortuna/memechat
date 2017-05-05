4888a29736897f6685cf793634dbc993956a5171
Collin Jackson <jackson@google.com>
2017-05-05 16:30:29 -0700

    Update to new project template

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,33 +1,28 @@
 #include "AppDelegate.h"
-#include "FirebaseDatabasePlugin.h"
-#include "FirebaseStoragePlugin.h"
-#include "GoogleSignInPlugin.h"
-#include "ImagePickerPlugin.h"
+#include "PluginRegistry.h"
 
 @implementation AppDelegate {
-  FirebaseDatabasePlugin *_firebaseDatabasePlugin;
-  FirebaseStoragePlugin *_firebaseStoragePlugin;
-  GoogleSignInPlugin *_googleSignInPlugin;
-  ImagePickerPlugin *_imagePickerPlugin;
+  PluginRegistry *plugins;
 }
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    FlutterViewController* flutterController = (FlutterViewController*)self.window.rootViewController;
-    _firebaseDatabasePlugin = [[FirebaseDatabasePlugin alloc] initWithFlutterView:flutterController];
-    _firebaseStoragePlugin = [[FirebaseStoragePlugin alloc] initWithFlutterView:flutterController];
-    _googleSignInPlugin = [[GoogleSignInPlugin alloc] initWithFlutterView:flutterController];
-    _imagePickerPlugin = [[ImagePickerPlugin alloc] initWithFlutterView:flutterController];
-    return YES;
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  // Override point for customization after application launch.
+  FlutterViewController *flutterController =
+      (FlutterViewController *)self.window.rootViewController;
+  plugins = [[PluginRegistry alloc] initWithController:flutterController];
+  return YES;
 }
 
 - (BOOL)application:(UIApplication *)app
             openURL:(NSURL *)url
             options:(NSDictionary *)options {
-  NSString *sourceApplication = options[UIApplicationOpenURLOptionsSourceApplicationKey];
+  NSString *sourceApplication =
+      options[UIApplicationOpenURLOptionsSourceApplicationKey];
   id annotation = options[UIApplicationOpenURLOptionsAnnotationKey];
-  return [_googleSignInPlugin handleURL:url
-                      sourceApplication:sourceApplication
-                             annotation:annotation];
+  return [plugins.google_sign_in handleURL:url
+                         sourceApplication:sourceApplication
+                                annotation:annotation];
 }
 
 @end
