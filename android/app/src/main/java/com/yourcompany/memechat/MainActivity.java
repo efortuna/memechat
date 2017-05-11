2c846759974a6a4ce3dfda68ac1612ac8369e03e
Emily Fortuna <efortuna@google.com>
2017-05-11 16:50:48 -0700

    Fix from plugin breaking change.

--- android/app/src/main/java/com/yourcompany/memechat/MainActivity.java
+++ android/app/src/main/java/com/yourcompany/memechat/MainActivity.java
@@ -3,22 +3,13 @@ package com.yourcompany.memechat;
 import android.content.Intent;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.PluginRegistry;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-    PluginRegistry pluginRegistry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        pluginRegistry = new PluginRegistry();
-        pluginRegistry.registerAll(this);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-      super.onActivityResult(requestCode, resultCode, data);
-      pluginRegistry.google_sign_in.onActivityResult(requestCode, resultCode, data);
-      pluginRegistry.image_picker.onActivityResult(requestCode, resultCode, data);
+        GeneratedPluginRegistrant.registerWith(this);
     }
 }
