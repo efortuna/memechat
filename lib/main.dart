546847083923ede591732782f2a318ec5436286f
Emily Shack <emshack@google.com>
2017-05-10 17:57:35 -0700

    Removed services.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
