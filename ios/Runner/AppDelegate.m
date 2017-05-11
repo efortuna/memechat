0c6e6907694b76033b1f3e7f0aec16c513b4ff3f
Emily Fortuna <efortuna@google.com>
2017-05-11 16:52:51 -0700

    Fix from plugin breaking change.

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,28 +1,13 @@
 #include "AppDelegate.h"
-#include "PluginRegistry.h"
+#include "GeneratedPluginRegistrant.h"
 
 @implementation AppDelegate {
-  PluginRegistry *plugins;
 }
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  // Override point for customization after application launch.
-  FlutterViewController *flutterController =
-      (FlutterViewController *)self.window.rootViewController;
-  plugins = [[PluginRegistry alloc] initWithController:flutterController];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
   return YES;
-}
-
-- (BOOL)application:(UIApplication *)app
-            openURL:(NSURL *)url
-            options:(NSDictionary *)options {
-  NSString *sourceApplication =
-      options[UIApplicationOpenURLOptionsSourceApplicationKey];
-  id annotation = options[UIApplicationOpenURLOptionsAnnotationKey];
-  return [plugins.google_sign_in handleURL:url
-                         sourceApplication:sourceApplication
-                                annotation:annotation];
 }
 
 @end
